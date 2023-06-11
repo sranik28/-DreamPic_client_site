aa73480dc926fa1dccca56ea2c4730d00171957d
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-06-11 16:58:20 +0600

    instructor privte added

--- src/private/InstructorOnly.jsx
+++ src/private/InstructorOnly.jsx
@@ -1,2 +1,21 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../../context/AuthProvider';
+import useAuthorization from '../../hooks/useAuthorization';
+
+
+
+const InstructorOnly = ({children}) => {
+    const {loading, user} = useAuth()
+    const {isLoading, role} = useAuthorization()
+    const location = useLocation();
+    if(loading || isLoading){
+        return 
+    }
+    
+    if(user.email && role === "instructor"){
+        return children;
+    }
+    return <Navigate state={{from: location}} to="/" replace/>;
+};
+
+export default InstructorOnly;
