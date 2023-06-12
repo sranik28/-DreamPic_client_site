52d5e6850e7067016c1a547c4d82b657fd655b73
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-06-12 19:55:46 +0600

    UI done

--- src/components/InstructorCard.jsx
+++ src/components/InstructorCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Fade } from 'react-awesome-reveal';
 
 const InstructorCard = ({ instructor }) => {
 
@@ -8,8 +9,10 @@ const InstructorCard = ({ instructor }) => {
         <div>
             <div className='flex flex-col items-center justify-center gap-4 p-4 bg-white rounded-md shadow-md'>
                 <img className='w-[200px] h-[200px] object-cover rounded-full' src={image} alt="" />
-                <h2 className='text-2xl font-semibold '>{name}</h2>
-                <p>{email}</p>
+                <Fade triggerOnce={.6} delay={300} direction="up" cascade>
+                    <h2 className='text-2xl font-semibold '>{name}</h2>
+                    <p>{email}</p>
+                </Fade>
             </div>
         </div>
     );
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,20 +1,12 @@
-import React, { useEffect } from 'react';
 import NavBar from '../pages/header/NavBar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
-import Aos from 'aos';
+
 
 
 const Root = () => {
 
-    useEffect(() => {
-        Aos.init(
-            {
-                duration:700,
-                disable: false,
-            }
-        )
-    }, [])
+  
 
     return (
         <div className='overflow-x-hidden'>
